4e1c88335547a79b4879435da00055caa379ea77
stephen-ka <stephen@n0clouds.com>
2021-10-25 13:44:19 -0600

    workstation complete

--- chef-lab/scripts/chefworkstation.sh
+++ chef-lab/scripts/chefworkstation.sh
@@ -3,3 +3,5 @@ wget https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/8/chef-
 sudo yum localinstall /tmp/chef-workstation-21.10.640-1.el8.x86_64.rpm -y
 echo eval $(chef -v)
 echo "complete chef workstation install" >> /home/vagrant/postscript.txt
+echo 'export PATH="/opt/chef-workstation/embedded/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'eval "$(chef shell-init bash)"' >> /home/vagrant/.bashrc
--- chef-lab/scripts/common.sh
+++ chef-lab/scripts/common.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 yum upgrade -y
-yum install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-yum install gcc kernel-devel kernel-headers dkms make bzip2 perl wget curl vim -y
+# yum install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+yum install epel-release gcc kernel-devel kernel-headers dkms make bzip2 perl wget curl vim -y
 echo "complete" >> /home/vagrant/postscript.txt
