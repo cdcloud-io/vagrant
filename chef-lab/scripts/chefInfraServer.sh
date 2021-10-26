9e0644c915ecd967fa76be595545288fd484e56b
stephen-ka <stephen@n0clouds.com>
2021-10-26 14:57:33 -0600

    update102321

--- chef-lab/scripts/chefInfraServer.sh
+++ chef-lab/scripts/chefInfraServer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 wget https://packages.chef.io/files/stable/chef-server/14.10.23/el/7/chef-server-core-14.10.23-1.el7.x86_64.rpm -P /tmp
-sudo yum localinstall /tmp/chef-workstation-21.10.640-1.el7.x86_64.rpm -y
+sudo yum localinstall /tmp/chef-server-core-14.10.23-1.el7.x86_64.rpm -y
+sleep 3
+sudo sudo chef-server-ctl reconfigure --chef-license accept-silent
+echo creating chef user: vagrant
+sleep 3
+sudo chef-server-ctl user-create vagrant vagrant user1 vagrant@n0clouds.com 'P@ssw0rd1!' --filename /home/vagrant/vagrant.pem
+echo Creating cheg-org: vagrant_org
+sleep 3
+sudo chef-server-ctl org-create vagrantorg 'vagrant_org' --association_user vagrant --filename /home/vagrant/vagrant_org-validator.pem
+
 
--- chef-lab/scripts/common.sh
+++ chef-lab/scripts/common.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
-yum upgrade -y
+# yum upgrade -y
 # yum install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum install epel-release gcc kernel-devel kernel-headers dkms make bzip2 perl wget curl vim -y
