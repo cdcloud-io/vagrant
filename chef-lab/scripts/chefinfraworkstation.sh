287d43ba29d5975becd8d5ff35a64cf3f5117f10
stephen-ka <stephen@n0clouds.com>
2021-10-26 07:59:39 -0600

    workstation update

---
+++ chef-lab/scripts/chefinfraworkstation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/7/chef-workstation-21.10.640-1.el7.x86_64.rpm -P /tmp
+yum localinstall https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/7/chef-workstation-21.10.640-1.el7.x86_64.rpm -y
+echo eval $(chef -v)
+echo "complete chef workstation install" >> /home/vagrant/postscript.txt
+echo 'export PATH="/opt/chef-workstation/bin/:/opt/chef-workstation/embedded/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'eval "$(chef shell-init bash)"' >> /home/vagrant/.bashrc
--- chef-lab/scripts/chefworkstation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/8/chef-workstation-21.10.640-1.el8.x86_64.rpm -P /tmp
-sudo yum localinstall /tmp/chef-workstation-21.10.640-1.el8.x86_64.rpm -y
-echo eval $(chef -v)
-echo "complete chef workstation install" >> /home/vagrant/postscript.txt
-echo 'export PATH="/opt/chef-workstation/embedded/bin:$PATH"' >> /home/vagrant/.bashrc
-echo 'eval "$(chef shell-init bash)"' >> /home/vagrant/.bashrc
