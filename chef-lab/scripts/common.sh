5dc87d94502dc1d52587bdaadc6e384c1ab56ed8
stephen-ka <stephen@n0clouds.com>
2021-10-26 08:37:12 -0600

    workstation update

--- chef-lab/scripts/common.sh
+++ chef-lab/scripts/common.sh
@@ -3,4 +3,3 @@
 yum upgrade -y
 # yum install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum install epel-release gcc kernel-devel kernel-headers dkms make bzip2 perl wget curl vim -y
-echo "complete" >> /home/vagrant/postscript.txt
