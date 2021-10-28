83f5b46432115367d1d1496bd4a1720d932d93e3
stephen_ka <stephen@cloudinitiatives.com>
2021-10-28 10:34:46 -0600

    final .1

--- chef-lab/scripts/chefinfraserver.sh
+++ chef-lab/scripts/chefinfraserver.sh
@@ -4,10 +4,15 @@ sudo yum localinstall /tmp/chef-server-core-14.10.23-1.el7.x86_64.rpm -y
 sleep 3
 sudo sudo chef-server-ctl reconfigure --chef-license accept-silent
 echo "creating chef admin: vagrant pw: P@ssw0rd1!"
+# sudo chef-server-ctl user-list
 sleep 3
 sudo chef-server-ctl user-create vagrant vagrant user1 vagrant@n0clouds.com 'P@ssw0rd1!' --filename /home/vagrant/vagrant.pem
 echo "Creating chef-org: vagrant_org"
-sleep 2
-sudo chef-server-ctl org-create vagrant-org 'vagrant-org' --association_user vagrant --filename /home/vagrant/vagrant-org-validator.pem
+# sudo chef-server-ctl org-list
+sleep 1
+sudo chef-server-ctl org-delete vagrantorg -y
+sleep 1
+sudo chef-server-ctl org-create vagrantorg 'vagrant org' --association_user vagrant --filename /home/vagrant/vagrantorg-validator.pem
 sudo chown vagrant:vagrant /home/vagrant/*.pem
-scp /home/vagrant/*.pem chefworkstation1:/home/vagrant/*
+scp /home/vagrant/*.pem chefworkstation1:/home/vagrant/
+
